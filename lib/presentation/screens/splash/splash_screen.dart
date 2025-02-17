3febdafdcdc9a2c68f40a7e4d550a656d60e2364
Akinsulire Olabode Solomon <solomonakinsulire@gmail.com>
2025-02-17 16:39:02 +0000

    feat: Created Splash Screen - Add splash screen file in 'lib/presentation/screens/splash'

---
+++ lib/presentation/screens/splash/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../../../core/theme/app_colors.dart'; // Adjust the import as needed
+import '../onboarding/onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Simulate a delay before navigating to the next screen (OnboardingScreen)
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        // ignore: use_build_context_synchronously
+        context,
+        MaterialPageRoute(builder: (context) => const OnboardingScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("../../../../assets/icon/Logo.png"),
+          ],
+        ),
+      ),
+    );
+  }
+}
