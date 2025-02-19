7767aa420f5511e483a582aa397ab8a53d4e04d4
Akinsulire Olabode Solomon <solomonakinsulire@gmail.com>
2025-02-19 13:39:06 +0000

    fix: code fixes

--- lib/presentation/screens/splash/splash_screen.dart
+++ lib/presentation/screens/splash/splash_screen.dart
@@ -1,6 +1,6 @@
+import 'package:baby_shop_hub/data/services/auth_gate.dart';
 import 'package:flutter/material.dart';
-import '../../../core/theme/app_colors.dart'; // Adjust the import as needed
-import '../onboarding/onboarding_screen.dart';
+import '../../../core/theme/app_colors.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
       Navigator.pushReplacement(
         // ignore: use_build_context_synchronously
         context,
-        MaterialPageRoute(builder: (context) => const OnboardingScreen()),
+        MaterialPageRoute(builder: (context) => const AuthGate()),
       );
     });
   }
