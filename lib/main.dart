c97b62ff08de9a08d76b3c9e6ee294257a2c471d
Akinsulire Olabode Solomon <solomonakinsulire@gmail.com>
2025-02-19 13:40:00 +0000

    chore(env): load and validate Supabase credentials using dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,29 @@
-// import 'package:baby_shop_hub/presentation/screens/onboarding/onboarding_screen.dart';
+import 'package:baby_shop_hub/presentation/screens/home/home_screen.dart';
 import 'package:baby_shop_hub/presentation/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Load environment variables
+  await dotenv.load(fileName: "../assets/.env");
+
+  // Get SUPABASE_URL and SUPABASE_ANON_KEY, ensuring they are not null
+  final supabaseUrl = dotenv.get("SUPABASE_URL", fallback: "");
+  final supabaseAnonKey = dotenv.get("SUPABASE_ANON_KEY", fallback: "");
+
+  if (supabaseUrl.isEmpty || supabaseAnonKey.isEmpty) {
+    throw Exception("Missing Supabase environment variables!");
+  }
+
+  // Initialize Supabase
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
+
   runApp(const MainApp());
 }
 
@@ -11,9 +32,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:SplashScreen(),
+      home: SplashScreen(),
+      routes: {
+        "/home": (context) => const HomeScreen(),
+      },
     );
   }
 }
