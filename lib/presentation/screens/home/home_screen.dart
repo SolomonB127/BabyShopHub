053b9515d3e068521db2b8e0198f858bdd56deee
Akinsulire Olabode Solomon <solomonakinsulire@gmail.com>
2025-02-21 14:26:47 +0000

    feat(home): integrate full-width search header in HomeScreen
    - Added a header with a reusable SearchTile and shopping cart icon.
    - Styled header to span full width with AppColors.primaryLight background.
    - Included a placeholder for featured products.

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,15 +1,67 @@
 import 'package:baby_shop_hub/core/theme/app_colors.dart';
+import 'package:baby_shop_hub/presentation/widgets/common/search_tile.dart';
 import 'package:flutter/material.dart';
-
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: AppColors.surface,
-      body: Center(child: Text("Home Screen"),),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              color: AppColors.primaryLight,
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: SearchTile(
+                      hintText: "Search Here",
+                      icon: IconButton(
+                        onPressed: () {
+                           // TODO - Add search functionality here.
+                        },
+                        icon: const Icon(Icons.search),
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+            
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: const Icon(
+                      Icons.shopping_cart,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Center(
+                  child: Text(
+                    "Featured Products",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
