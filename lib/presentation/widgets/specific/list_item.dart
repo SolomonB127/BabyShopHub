49e2c99d4c0ac54e5e2677699326ddd786d34122
Akinsulire Olabode Solomon <solomonakinsulire@gmail.com>
2025-02-26 09:46:43 +0000

    feat: Add dynamic onTap functio

--- lib/presentation/widgets/specific/list_item.dart
+++ lib/presentation/widgets/specific/list_item.dart
@@ -3,16 +3,20 @@ import 'package:flutter/material.dart';
 class ListItem extends StatelessWidget {
   final String title;
   final String? trailingText;
-  const ListItem({super.key, required this.title, this.trailingText});
+  final void Function()? onTap;
+  const ListItem({super.key, required this.title, this.trailingText, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(title, style: const TextStyle(fontSize: 16)),
-      trailing: trailingText != null
-          ? Text(trailingText!,
-              style: TextStyle(color: Colors.grey.shade600, fontSize: 14))
-          : const Icon(Icons.arrow_forward_ios, size: 16, color: Colors.grey),
+    return GestureDetector(
+      onTap: onTap,
+      child: ListTile(
+        title: Text(title, style: const TextStyle(fontSize: 16)),
+        trailing: trailingText != null
+            ? Text(trailingText!,
+                style: TextStyle(color: Colors.grey.shade600, fontSize: 14))
+            : const Icon(Icons.arrow_forward_ios, size: 16, color: Colors.grey),
+      ),
     );
   }
 }
